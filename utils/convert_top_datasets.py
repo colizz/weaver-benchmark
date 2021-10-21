21b31977a34742b6fb79ac4fcba4ccb789c4a6bc
Congqiao Li <congqiao.li@cern.ch>
2021-10-22 00:43:08 +0800

    Add options to provide input file path and ROOT writing methods

--- utils/convert_top_datasets.py
+++ utils/convert_top_datasets.py
@@ -1,42 +1,114 @@
 import pandas as pd
+import awkward as ak
 import numpy as np
-import awkward0 as ak0
 import math
+import os
 from tqdm import tqdm
 import argparse
 parser = argparse.ArgumentParser('Convert top benchmark h5 datasets to awkd')
-parser.add_argument('-c', '--condition', required=True, help='Create dataset for training/test. Set to \'train\' or \'test\'.')
-parser.add_argument('--max-event-size', default=100000, help='Maximum event size per output file.')
+parser.add_argument('-i', '--inputdir', required=True, help='Directory that contains the input h5 file.')
+parser.add_argument('-c', '--condition', default='all', choices=['train', 'val', 'test', 'all'], help='Create dataset for train/test/val/all.')
+parser.add_argument('-m', '--mode', default='uproot', choices=['awkd', 'uproot', 'ROOT'], help='Mode to write ROOT files')
+parser.add_argument('--max-event-size', type=int, default=100000, help='Maximum event size per output file.')
 args = parser.parse_args()
 
-if args.condition == 'train':
-    inputfiles, outputfile = ['train.h5'], 'top_train'
-elif args.condition == 'val':
-    inputfiles, outputfile = ['val.h5'], 'top_val'
-elif args.condition == 'test':
-    inputfiles, outputfile = ['test.h5'], 'top_test'
-else:
-    raise RuntimeError('Invalid --condition option')
 
-def store_file(res_array, outname):
-    print('Saving to file', outname, '...')
-    ak0.save(outname, ak0.fromiter(res_array), mode='w')
+def store_file_awkd(res_array_2d, res_array_1d, outpath):
+    r"""Write .awkd files with awkward0
+    """
+    import awkward0 as ak0
+    outpath += '.awkd'
+    print('Saving to file', outpath, '...')
+    if not os.path.exists(os.path.dirname(outpath)):
+        os.makedirs(os.path.dirname(outpath))
+    ak0.save(outpath, ak0.fromiter({**res_1d, **{'Jet_'+k: res_2d[k] for k in res_2d}} for res_1d, res_2d in zip(res_array_1d, res_array_2d)), mode='w')
  
-def main():
+
+def store_file_uproot(res_array_2d, res_array_1d, outpath):
+    r"""Write ROOT files with the latest feature in uproot(4)
+    """
+    import uproot
+    def _check_uproot_version(uproot):
+        v = uproot.__version__.split('.')
+        v = int(v[0])*10000 + int(v[1])*100 + int(v[2])
+        assert v >= 40104, "Uproot version should be >= 4.1.4 for the stable uproot-writing feature"
+
+    _check_uproot_version(uproot)
+    outpath += '.root'
+    print('Saving to file', outpath, '...')
+    ak_array2d = ak.from_iter(res_array_2d)
+    ak_array1d = ak.from_iter(res_array_1d)
+    if not os.path.exists(os.path.dirname(outpath)):
+        os.makedirs(os.path.dirname(outpath))
+    with uproot.recreate(outpath, compression=uproot.LZ4(4)) as fw:
+        # Note that 2D variable names prefixed with `Jet_` due to uproot storing rule of jagged arrays
+        fw['Events'] = {'Jet': ak.zip({k:ak_array2d[k] for k in ak.fields(ak_array2d)}), **{k:ak_array1d[k] for k in ak.fields(ak_array1d)}}
+        fw['Events'].title = 'Events'
+
+
+def store_file_ROOT(res_array_2d, res_array_1d, outpath):
+    r"""Write ROOT files with PyROOT
+    """
+    import ROOT
+    from array import array
+    ROOT.ROOT.EnableImplicitMT(4)
+    outpath += '.root'
+    print('Saving to file', outpath, '...')
+    if not os.path.exists(os.path.dirname(outpath)):
+        os.makedirs(os.path.dirname(outpath))
+    f = ROOT.TFile(outpath, 'recreate')
+    f.SetCompressionAlgorithm(ROOT.kLZ4)
+    f.SetCompressionLevel(4)
+    try:
+        tree = ROOT.TTree('Events', 'Events')
+        # Reserve branches for in TTree
+        dic = {}
+        for var in res_array_1d[0]:
+            vartype = 'i' if 'int' in str(type(res_array_1d[0][var])) else 'f'
+            dic[var] = array(vartype, [1])
+            tree.Branch(var, dic[var], f'{var}/{vartype.upper()}')
+        for var in res_array_2d[0]:
+            dic['Jet_' + var] = ROOT.vector('float')()
+            tree.Branch('Jet_' + var, 'vector<float>', dic['Jet_' + var])
+        # Store variables
+        for res_1d, res_2d in zip(res_array_1d, res_array_2d): # loop event by event
+            for var in res_1d: # loop over variable names
+                dic[var][0] = res_1d[var]
+            for var in res_2d:
+                dic['Jet_' + var].clear()
+                for v in res_2d[var]:
+                    dic['Jet_' + var].push_back(v)
+            tree.Fill()
+        f.Write()
+    finally:
+        f.Close()
+
+
+def convert(input_files, output_file, store_file_func):
+    #  List all 2D and 1D variables to store. Note that 2D variables will be prefixed with `Jet_`
     varlist_2d = ['E', 'PX', 'PY', 'PZ']
     varlist_1d = ['truthE', 'truthPX', 'truthPY', 'truthPZ', 'ttv', 'is_signal_new']
     varlist_2d_new = ['E_log', 'P', 'P_log', 'Etarel', 'Phirel']
     varlist_1d_new = ['E_tot', 'PX_tot', 'PY_tot', 'PZ_tot', 'P_tot', 'Eta_tot', 'Phi_tot']
     idx, ifile = 0, 0
-    res_array = []
-    for filename in inputfiles:
+    res_array_2d, res_array_1d = [], []
+    for filename in input_files:
         print('Reading table from:', filename, '...')
-        with pd.HDFStore(f'samples/{filename}') as store:
+        with pd.HDFStore(os.path.join(args.inputdir, filename)) as store:
             df = store.select('table')
 
         print('Processing events ...')
         isfirst = True
         for origIdx, row in tqdm(df.iterrows()):
+
+            if idx >= args.max_event_size:
+                # Reach the max event limit per file. Store the current arrays into file
+                store_file_func(res_array_2d, res_array_1d, os.path.join(args.inputdir, 'prep', f'{output_file}_{ifile}'))
+                del res_array_2d, res_array_1d
+                res_array_2d, res_array_1d = [], []
+                ifile += 1
+                idx = 0
+
             # First initiate 2d arrays
             res = {k:[] for k in varlist_2d + varlist_2d_new}
             nPart = 0
@@ -72,23 +144,30 @@ def main():
                 res['Phirel'].append(Phi - res['Phi_tot'] - 2 * math.pi * math.floor((Phi - res['Phi_tot']) / (2 * math.pi) + 0.5))
 
             # Store per event result
-            res_array.append(res)
-            idx += 1
-                        
+            res_array_2d.append({k:res[k] for k in varlist_2d + varlist_2d_new})
+            res_array_1d.append({k:res[k] for k in res.keys() if k not in varlist_2d + varlist_2d_new})
+
             if isfirst:
                 print(res)
                 isfirst = False
 
-            if idx >= args.max_event_size:
-                store_file(res_array, f'{outputfile}_{ifile}.awkd')
-                del res_array
-                res_array = []
-                ifile += 1
-                idx = 0
+            idx += 1
 
     # Save rest of events before finishing
-    store_file(res_array, f'{outputfile}_{ifile}.awkd')
+    store_file_func(res_array_2d, res_array_1d, os.path.join(args.inputdir, 'prep', f'{output_file}_{ifile}'))
 
 
 if __name__ == '__main__':
-    main()
+    store_file_func = store_file_awkd if args.mode == 'awkd' else \
+        store_file_uproot if args.mode == 'uproot' else \
+        store_file_ROOT if args.mode == 'ROOT' else None
+    if args.condition == 'train':
+        convert(input_files=['train.h5'], output_file='top_train', store_file_func=store_file_func)
+    elif args.condition == 'val':
+        convert(input_files=['val.h5'], output_file='top_val', store_file_func=store_file_func)
+    elif args.condition == 'test':
+        convert(input_files=['test.h5'], output_file='top_test', store_file_func=store_file_func)
+    elif args.condition == 'all':
+        convert(input_files=['train.h5'], output_file='top_train', store_file_func=store_file_func)
+        convert(input_files=['val.h5'], output_file='top_val', store_file_func=store_file_func)
+        convert(input_files=['test.h5'], output_file='top_test', store_file_func=store_file_func)
